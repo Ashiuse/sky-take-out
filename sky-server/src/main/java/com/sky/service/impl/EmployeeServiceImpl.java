9f97abef12969e13e06b1aa4f9ef9c52e5954b42
Ashiuse <3026643165@qq.com>
2025-10-21 15:21:34 +0800

    新增员工

--- sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
@@ -2,6 +2,8 @@ package com.sky.service.impl;
 
 import com.sky.constant.MessageConstant;
 import com.sky.constant.StatusConstant;
+import com.sky.context.BaseContext;
+import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
 import com.sky.entity.Employee;
 import com.sky.exception.AccountLockedException;
@@ -9,9 +11,12 @@ import com.sky.exception.AccountNotFoundException;
 import com.sky.exception.PasswordErrorException;
 import com.sky.mapper.EmployeeMapper;
 import com.sky.service.EmployeeService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.DigestUtils;
+
+import java.time.LocalDateTime;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
@@ -38,8 +43,8 @@ public class EmployeeServiceImpl implements EmployeeService {
             throw new AccountNotFoundException(MessageConstant.ACCOUNT_NOT_FOUND);
         }
 
-        //密码比对
-        // TODO 后期需要进行md5加密，然后再进行比对
+        //对前端传入的明文密码进行md5加密，然后再进行比对
+        password = DigestUtils.md5DigestAsHex(password.getBytes());
         if (!password.equals(employee.getPassword())) {
             //密码错误
             throw new PasswordErrorException(MessageConstant.PASSWORD_ERROR);
@@ -52,6 +57,26 @@ public class EmployeeServiceImpl implements EmployeeService {
 
         //3、返回实体对象
         return employee;
+    }
+
+    //新增员工
+    @Override
+    public void save(EmployeeDTO employeeDTO) {
+        Employee employee = new Employee();
+
+        //拷贝属性
+        BeanUtils.copyProperties(employeeDTO, employee);
+
+        //设置其他属性
+        employee.setStatus(StatusConstant.ENABLE);//账号状态(默认:启用)
+        employee.setPassword(DigestUtils.md5DigestAsHex("123456".getBytes()));//密码(初始密码:123456)
+        employee.setCreateTime(LocalDateTime.now());//创建时间
+        employee.setUpdateTime(LocalDateTime.now());//修改时间
+        employee.setCreateUser(BaseContext.getCurrentId());//创建人ID
+        employee.setUpdateUser(BaseContext.getCurrentId());//修改人ID
+
+        employeeMapper.insert(employee);
+
     }
 
 }
