1f15cdc4c2a483922a677997a21fdf4682ba9c5f
Ashiuse <3026643165@qq.com>
2025-10-22 17:56:20 +0800

    小修插入菜品

--- sky-server/src/main/java/com/sky/mapper/DishFlavorMapper.java
+++ sky-server/src/main/java/com/sky/mapper/DishFlavorMapper.java
@@ -2,7 +2,6 @@ package com.sky.mapper;
 
 import com.sky.entity.DishFlavor;
 import org.apache.ibatis.annotations.Delete;
-import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
@@ -12,9 +11,9 @@ import java.util.List;
 public interface DishFlavorMapper {
     /**
      * 批量插入口味数据
-     * @param dishFlavors
+     * @param flavors
      */
-    void insertBatch(List<DishFlavor> dishFlavors);
+    void insertBatch(List<DishFlavor> flavors);
 
     /**
      * 根据菜品id删除对应的口味数据
