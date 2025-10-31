9bd7788a540013f432125f8f25d3ae6ca40bafe2
Ashiuse <3026643165@qq.com>
2025-10-31 14:38:44 +0800

    新增用户和用户总量统计

--- sky-server/src/main/java/com/sky/controller/admin/ReportController.java
+++ sky-server/src/main/java/com/sky/controller/admin/ReportController.java
@@ -3,6 +3,7 @@ package com.sky.controller.admin;
 import com.sky.result.Result;
 import com.sky.service.ReportService;
 import com.sky.vo.TurnoverReportVO;
+import com.sky.vo.UserReportVO;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
@@ -38,8 +39,21 @@ public class ReportController {
                                                        LocalDate begin,
                                                        @DateTimeFormat(pattern = "yyyy-MM-dd")
                                                        LocalDate end) {
-        log.info("统计营业额数据：{}到{}", begin, end);
         return Result.success(reportService.getTurnover(begin, end));
+    }
+
+    /**
+     * 统计用户数据
+     *
+     * @return
+     */
+    @GetMapping("/userStatistics")
+    @ApiOperation("统计用户数据")
+    public Result<UserReportVO> userStatistics(@DateTimeFormat(pattern = "yyyy-MM-dd")
+                                               LocalDate begin,
+                                               @DateTimeFormat(pattern = "yyyy-MM-dd")
+                                               LocalDate end) {
+        return Result.success(reportService.getUserStatistics(begin, end));
     }
 
 
--- sky-server/src/main/java/com/sky/service/ReportService.java
+++ sky-server/src/main/java/com/sky/service/ReportService.java
@@ -1,6 +1,7 @@
 package com.sky.service;
 
 import com.sky.vo.TurnoverReportVO;
+import com.sky.vo.UserReportVO;
 
 import java.time.LocalDate;
 
@@ -8,10 +9,17 @@ public interface ReportService {
 
     /**
      * 根据时间区间查询营业额数据
-     * @param beginTime
-     * @param endTime
+     * @param begin
+     * @param end
      * @return
      */
-    TurnoverReportVO getTurnover(LocalDate beginTime, LocalDate endTime);
+    TurnoverReportVO getTurnover(LocalDate begin, LocalDate end);
 
+    /**
+     * 根据时间区间查询用户数据
+     * @param begin
+     * @param end
+     * @return
+     */
+    UserReportVO getUserStatistics(LocalDate begin, LocalDate end);
 }
